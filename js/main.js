718905e70ff5448d85ac3a43894adc2d16bae548
Zira Serrano <ziraserrano@gmail.com>
2022-10-06 11:51:51 -0400

    Add input reset to function

--- js/main.js
+++ js/main.js
@@ -2,24 +2,27 @@
 
 document.querySelector('button').addEventListener('click', getAPOD)
 
+
 function getAPOD(){
-    const date = document.querySelector('input').value
-    const url = `https://api.nasa.gov/planetary/apod?api_key=JWydXxvFCaUjBZtN2qOcME0pFeBgeMeoSwDhhxkt&date=${date}`
+  const date = document.querySelector('input').value
+  const url = `https://api.nasa.gov/planetary/apod?api_key=JWydXxvFCaUjBZtN2qOcME0pFeBgeMeoSwDhhxkt&date=${date}`
+  document.querySelector('input').value = ""
 
     fetch(url)
     .then(res => res.json()) // parse response as JSON
     .then(data => {
       console.log(data)
-      if( data.media_type === 'image' ){
+      if( data.media_type === 'image' ){   
         document.querySelector('img').src = data.hdurl
       }else if(data.media_type === 'video'){
         document.querySelector('iframe').src = data.url
       }
-     
+      
       document.querySelector('#description').innerText = data.explanation
     })
     .catch(err => {
-        console.log(`error ${err}`)
+      console.log(`error ${err}`)
     });
-
-}
+    
+  }
+  
